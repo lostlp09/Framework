b4bbd7d9d18880afc4cced4ee09076d78b86080b
lostlp09 <mrnootnoot09@gmail.com>
2025-11-19 20:49:55 +0100

    creating password screen

--- framework/character_body_3d.gd
+++ framework/character_body_3d.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 var xrotation 
 @onready var camera = $Camera3D
+@onready var leptop = $"../Sketchfab_Scene"
+@onready var interact = $"../Control/Label"
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	xrotation = camera.rotation_degrees.x
@@ -15,12 +17,21 @@ func _input(event: InputEvent) -> void:
 		self.rotation.y -= event.relative.x * 0.001
 
 func _physics_process(delta: float) -> void:
+
+	var distance = self.position.distance_to(leptop.position)
+	if  distance <= 3:
+		if Input.is_action_just_pressed("interact"):
+			get_tree().change_scene_to_file("res://2d.tscn")
+			Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+		interact.visible = true
+	else:
+		interact.visible = false
 	var movment = Vector3.ZERO
 
 	var leftright = Input.get_axis("left","right")
 	var forbackwards = Input.get_axis("forward","backwardss")
-
 	
+	movment.y = -30
 	movment.x = leftright *-600 * delta
 	movment.z = forbackwards *-600 * delta
 	movment = movment.rotated(Vector3.UP,self.rotation.y)
