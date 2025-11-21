f1ebb6db153674d6d4c81c7bf59f97f0e5368f52
lostlp09 <mrnootnoot09@gmail.com>
2025-11-21 18:41:58 +0100

    gamestore
    added playable games (yipee

--- framework/gamestore.gd
+++ framework/gamestore.gd
@@ -1,18 +1,23 @@
 extends ScrollContainer
 
-@onready var object =$VBoxContainer/Panel/Sprite2D
-var links = 0
+@onready var object =preload("res://label.tscn")
+var gamenameandlinks = [{"gamename":"Magicfight","Url":"https://lostlp09.itch.io/magic-parkour"},
+	{"gamename":"gridbuilder","Url":"https://lostlp09.itch.io/gridbuilder"},
+	{"gamename":"ballfall","Url":"https://lostlp09.itch.io/ball-fall"},
+	{"gamename":"Signalrobotgame","Url":"https://lostlp09.itch.io/signal-roblot-game"},
+	{"gamename":"flagguesser","Url":"https://lostlp09.itch.io/flagguesser"},
+	{"gamename":"coinclickerx","Url":"https://lostlp09.itch.io/coinclickerx"}
+	]
 func _ready() -> void:
 	generatebuttons()
 	pass # Replace with function body.
 var gameurl
 func generatebuttons()->void:
-	var index = 0
-	var startpos = Vector2(0,156)
+	var startpos = Vector2(-100,156)
 	var anzahl = 1
-	for i in range(0,10):
-		index += 1
-		var clone:Sprite2D= object.duplicate()
+	for i in gamenameandlinks:
+		
+		var clone:Label= object.instantiate()
 		if anzahl == 5:
 			anzahl = 1
 			startpos.y += 400
@@ -21,5 +26,7 @@ func generatebuttons()->void:
 		$VBoxContainer/Panel.add_child(clone)
 		anzahl +=1
 		print(clone.position)
-
-func onplaypressed()->void:
+		clone.get_node("Button").pressed.connect(onplaypressed.bind(i["Url"]))
+		clone.text = "            " + i["gamename"]
+func onplaypressed(gameurl)->void:
+	OS.shell_open(gameurl)
